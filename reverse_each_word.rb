147e520279eb55575d7c91d42b28c868b368d789
Ebo Lee <yiboli.coding@gmail.com>
2020-07-08 22:25:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   ##array = sentence.split(//)
   ##return array.reverse
-  sentence.split(" ").each {|word| word.re}
+  sentence.split(" ").collect {|word| word.re}
 end
